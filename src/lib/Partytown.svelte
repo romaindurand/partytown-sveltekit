be17037bb1048272b42f7447e04cef982e8dcc9a
Romain Durand <romain.durand@radiofrance.com>
2022-04-07 16:33:57 +0200

    initial commit

---
+++ src/lib/Partytown.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let script = '';
+
+	$: partytownScript = `<sc` + `ript type="script/partytown">${script}</scr` + `ipt>`;
+</script>
+
+{@html partytownScript}
---
+++ src/lib/PartytownSnippet.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { partytownSnippet } from '@builder.io/partytown/integration';
+	const partytown = partytownSnippet();
+</script>
+
+<svelte:head>
+	{@html `<scr` + `ipt>${partytown}</script>`}
+</svelte:head>
